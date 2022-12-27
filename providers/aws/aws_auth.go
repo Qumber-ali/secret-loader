d61478088cd55b3b34bf164d22f0f2543de8a309
Qumber-ali <syedqamberaliraza@JCI-Mac.local>
2022-12-27 12:07:07 +0500

    Added read region(not available in aws sdk for golang) from shared config ability.

--- providers/aws/aws_auth.go
+++ providers/aws/aws_auth.go
@@ -2,14 +2,25 @@ package aws_secrets_manager
 
 import (
 	"fmt"
+        "context"
+        "github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
+        "github.com/aws/aws-sdk-go-v2/config"
 	"os"
 )
 
 func Auth(profile string) *session.Session {
 
+        ctx := context.Background()
+       
+        shared_config, err := config.LoadSharedConfigProfile(ctx, profile)
+
 	sess, err := session.NewSessionWithOptions(session.Options{
 		Profile: profile,
+
+                Config: aws.Config {
+                  Region: aws.String(shared_config.Region),
+                },
 	})
 
 	if err != nil {
